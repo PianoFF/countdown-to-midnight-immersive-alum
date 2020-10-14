1df8ff8efebf890bb7cc554ec22a136922677b9c
pianoFF <fangfei1star@gmail.com>
2020-10-14 12:05:49 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+require "timeout"
+require "pry"
+
+def countdown (seconds_left)
+  while seconds_left > 0
+    puts "#{seconds_left} SECOND(S)!"
+    seconds_left -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep(seconds)
 end
